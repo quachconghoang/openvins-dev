3a0a37a99d2597ae4a4acaf621a4173cf851bdc8
quachconghoang <hoangqc@vnu.edu.vn>
2023-04-21 13:41:18 +1000

    update scripts

--- ov_msckf/scripts/run_sim_tartanair.sh
+++ ov_msckf/scripts/run_sim_tartanair.sh
@@ -1,48 +1,79 @@
 #!/usr/bin/env bash
 
 # Source our workspace directory to load ENV variables
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo $SCRIPT_DIR
 source ${SCRIPT_DIR}/../../../../devel/setup.bash
 echo ${SCRIPT_DIR}/../../../../devel/setup.bash
 
-#dataset_carwelding=("P001"  "P002"  "P004"  "P005"  "P006"  "P007")
-#dataset_neighborhood=("P000"  "P001"  "P002"  "P003"  "P004"  "P005")
-#dataset_office=("P000"  "P001"  "P002"  "P003"  "P004"  "P005"  "P006")
-#dataset_oldtown=("P000"  "P001"  "P002"  "P004"  "P005"  "P007")
+dataset_carwelding_easy=("P001" "P002" "P004" "P005" "P006" "P007")
+dataset_carwelding_hard=("P000" "P001" "P002" "P003")
 
-#for i in "${!dataset_carwelding[@]}"; do
+dataset_abandonedfactory_easy=("P000" "P001" "P002" "P004" "P005" "P006" "P008" "P009" "P010" "P011")
+dataset_abandonedfactory_hard=("P000" "P001" "P002" "P003" "P004" "P005" "P006" "P007" "P008" "P009" "P010" "P011")
+
+dataset_office_easy=("P000"  "P001"  "P002"  "P003"  "P004"  "P005"  "P006")
+dataset_office_hard=("P000"  "P001"  "P002"  "P003"  "P004"  "P005"  "P006" "P007")
+
+dataset_office2_easy=("P000" "P003" "P004" "P005" "P006" "P007" "P008" "P009" "P010" "P011")
+dataset_office2_hard=("P000" "P001" "P002" "P003" "P004" "P005" "P006" "P007" "P008" "P009" "P010")
+
+
+#for i in "${!dataset_carwelding_hard[@]}"; do
 #roslaunch ov_msckf sim_tartanair.launch \
 #  data_scenario:="carwelding" \
-#  dataset:="${dataset_carwelding[i]}" \
+#  data_level:="Hard" \
+#  dataset:="${dataset_carwelding_hard[i]}" \
 #  verbosity:="SILENT"
 #done
 #
-#for i in "${!dataset_neighborhood[@]}"; do
+#for i in "${!dataset_abandonedfactory_hard[@]}"; do
 #roslaunch ov_msckf sim_tartanair.launch \
-#  data_scenario:="neighborhood" \
-#  dataset:="${dataset_neighborhood[i]}" \
+#  data_scenario:="abandonedfactory" \
+#  data_level:="Hard" \
+#  dataset:="${dataset_abandonedfactory_hard[i]}" \
 #  verbosity:="SILENT"
 #done
 #
-#for i in "${!dataset_office[@]}"; do
+#for i in "${!dataset_office_hard[@]}"; do
 #roslaunch ov_msckf sim_tartanair.launch \
 #  data_scenario:="office" \
-#  dataset:="${dataset_office[i]}" \
+#  data_level:="Hard" \
+#  dataset:="${dataset_office_hard[i]}" \
 #  verbosity:="SILENT"
 #done
 #
-#for i in "${!dataset_oldtown[@]}"; do
+#for i in "${!dataset_office2_hard[@]}"; do
 #roslaunch ov_msckf sim_tartanair.launch \
-#  data_scenario:="oldtown" \
-#  dataset:="${dataset_oldtown[i]}" \
+#  data_scenario:="office2" \
+#  data_level:="Hard" \
+#  dataset:="${dataset_office2_hard[i]}" \
 #  verbosity:="SILENT"
 #done
 
-dataset_carwelding_hard=("P000"   "P001"  "P002"  "P003")
-dataset_neighborhood_hard=("P000"  "P001"  "P002"  "P003"  "P004"  "P005"  "P006")
-dataset_office_hard=("P000"  "P001"  "P002"  "P003"  "P004"  "P005"  "P006" "P007")
-dataset_oldtown_hard=("P000"  "P001"  "P002"   "P003"  "P004"  "P005"  "P006"  "P007" "P008")
+for i in "${!dataset_abandonedfactory_easy[@]}"; do
+roslaunch ov_msckf sim_tartanair.launch \
+  data_scenario:="abandonedfactory" \
+  data_level:="Easy" \
+  dataset:="${dataset_abandonedfactory_easy[i]}" \
+  verbosity:="SILENT"
+done
+
+for i in "${!dataset_abandonedfactory_hard[@]}"; do
+roslaunch ov_msckf sim_tartanair.launch \
+  data_scenario:="abandonedfactory" \
+  data_level:="Hard" \
+  dataset:="${dataset_abandonedfactory_hard[i]}" \
+  verbosity:="SILENT"
+done
+
+for i in "${!dataset_carwelding_easy[@]}"; do
+roslaunch ov_msckf sim_tartanair.launch \
+  data_scenario:="carwelding" \
+  data_level:="Easy" \
+  dataset:="${dataset_carwelding_easy[i]}" \
+  verbosity:="SILENT"
+done
 
 for i in "${!dataset_carwelding_hard[@]}"; do
 roslaunch ov_msckf sim_tartanair.launch \
@@ -52,11 +83,11 @@ roslaunch ov_msckf sim_tartanair.launch \
   verbosity:="SILENT"
 done
 
-for i in "${!dataset_neighborhood_hard[@]}"; do
+for i in "${!dataset_office_easy[@]}"; do
 roslaunch ov_msckf sim_tartanair.launch \
-  data_scenario:="neighborhood" \
-  data_level:="Hard" \
-  dataset:="${dataset_neighborhood_hard[i]}" \
+  data_scenario:="office" \
+  data_level:="Easy" \
+  dataset:="${dataset_office_easy[i]}" \
   verbosity:="SILENT"
 done
 
@@ -68,10 +99,18 @@ roslaunch ov_msckf sim_tartanair.launch \
   verbosity:="SILENT"
 done
 
-for i in "${!dataset_oldtown_hard[@]}"; do
+for i in "${!dataset_office2_easy[@]}"; do
 roslaunch ov_msckf sim_tartanair.launch \
-  data_scenario:="oldtown" \
+  data_scenario:="office2" \
+  data_level:="Easy" \
+  dataset:="${dataset_office2_easy[i]}" \
+  verbosity:="SILENT"
+done
+
+for i in "${!dataset_office2_hard[@]}"; do
+roslaunch ov_msckf sim_tartanair.launch \
+  data_scenario:="office2" \
   data_level:="Hard" \
-  dataset:="${dataset_oldtown_hard[i]}" \
+  dataset:="${dataset_office2_hard[i]}" \
   verbosity:="SILENT"
 done
